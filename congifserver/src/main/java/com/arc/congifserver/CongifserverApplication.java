5c5e34a40d633a71f9fe44c9f12d44977cae4740
AbQaadir <qaadir.inbox@gmail.com>
2024-03-01 05:29:21 +0530

    Development configration server created for review-micro service only *-*

--- congifserver/src/main/java/com/arc/congifserver/CongifserverApplication.java
+++ congifserver/src/main/java/com/arc/congifserver/CongifserverApplication.java
@@ -2,8 +2,10 @@ package com.arc.congifserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CongifserverApplication {
 
 	public static void main(String[] args) {
