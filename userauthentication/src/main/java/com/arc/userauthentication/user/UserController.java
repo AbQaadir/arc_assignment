7149c217fb2dc4b1704229526c090e029e047db6
AbQaadir <qaadir.inbox@gmail.com>
2024-03-01 07:24:43 +0530

    Gateway Micro service added. working

--- userauthentication/src/main/java/com/arc/userauthentication/user/UserController.java
+++ userauthentication/src/main/java/com/arc/userauthentication/user/UserController.java
@@ -6,6 +6,7 @@ import com.arc.userauthentication.dto.UserDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
